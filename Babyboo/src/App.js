54a598b7c23879ccfb5b4eb88813a04b267cce93
LyKhaiNghiep <khainghieply@gmail.com>
2024-06-13 11:58:55 +0700

    Add environment
    add .env file, api link and react query to project

--- Babyboo/src/App.js
+++ Babyboo/src/App.js
@@ -1,27 +1,42 @@
-import React, { Fragment } from 'react'
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import {routes} from './routes'
-import DefaultComponent from './components/DefaultComponent/DefaultComponent'
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { routes } from "./routes";
+import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
+
+  const fetchApi = async () => {
+    const res = axios.get(`${process.env.REACT_APP_API_URL}/products`);
+    return (await res).data;
+  };
+
+  const query = useQuery({ queryKey: ["todos"], queryFn: fetchApi });
+
+  console.log('query',query)
   return (
     <div>
-        <Router>
-          <Routes>
-            {routes.map((route) => {
-              const Page = route.page
-              const Layout = route.isShowHeader ? DefaultComponent : Fragment
-              return(
-                <Route key={route.path} path={route.path} element={
-                <Layout>
-                <Page />
-                 </Layout>
-              } />
-              )
-              })}
-          </Routes>
-        </Router>
+      <Router>
+        <Routes>
+          {routes.map((route) => {
+            const Page = route.page;
+            const Layout = route.isShowHeader ? DefaultComponent : Fragment;
+            return (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
-export default App
+export default App;
